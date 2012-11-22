5021eda4fb42cc9b07b2cdb7e605e36385ca926d
awinabi <awin.abi@strata.co.in>
2012-11-22 20:58:50 +0530

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundbeats::Application.config.secret_token = '81ca6c294e41bf49aa89c3bad3e7b5ffe2e755905a94e473b35641848f1d4ce1cc1af011c410bfcd8bfd6d337bfda8aff668b194eb8a3db0d71cc89b748af9e5'
