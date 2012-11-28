0693d6967d201c4441b2ce76e07c5600cfeefb36
Awin Abi <awinabi@gmail.com>
2012-11-28 21:41:34 +0530

    adding yield for page specific css/js

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121122162928) do
+ActiveRecord::Schema.define(:version => 20121128160632) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "artists", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
