29ad2a8b8b3bf0f5b38f042bbdd8b728c6a45c6e
A D Vishnu Prasad <advishnuprasad@gmail.com>
2013-01-14 18:50:54 +0530

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Soundbeats::Application.routes.draw do
-  # devise_for :users
-
-  devise_scope :user do
-    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
-  end
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users
 
   resources :students
   resources :result
